fbcda854e07270242917aef1f63b5387518621e0
Eric Söderqvist <70255364+EricSoder@users.noreply.github.com>
2023-10-23 14:39:29 +0200

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -3,8 +3,8 @@ const core = require("@actions/core");
 
 async function run() {
   const joke = await getJoke();
-  //console.log(joke);
-  core.setOutput("joke-output", joke);
+  console.log(joke);
+  //core.setOutput("joke-output", joke);
 }
 
 run();
